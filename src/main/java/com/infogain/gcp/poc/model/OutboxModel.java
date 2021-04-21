150de45cd3d4ce691082cddf5547d43751ced0c2
Hemanshu Gupta <Hemanshu.Gupta@igglobal.com>
2021-04-21 09:29:04 +0530

    Added created in payload

--- src/main/java/com/infogain/gcp/poc/model/OutboxModel.java
+++ src/main/java/com/infogain/gcp/poc/model/OutboxModel.java
@@ -11,16 +11,20 @@ import org.apache.commons.beanutils.BeanUtils;
 @ToString
 public class OutboxModel {
 
-        private String locator;
+    private String locator;
     private Integer version;
     private String parent_locator;
+    private String created;
     private String data;
 
     @SneakyThrows
     public OutboxEntity buildEntity(){
         OutboxEntity outboxEntity = new OutboxEntity();
-        BeanUtils.copyProperties(outboxEntity, this);
-        outboxEntity.setCreated(Timestamp.now());
+        outboxEntity.setData(data);
+        outboxEntity.setLocator(locator);
+        outboxEntity.setParent_locator(parent_locator);
+        outboxEntity.setVersion(version);
+        outboxEntity.setCreated(Timestamp.parseTimestamp(created));
         return outboxEntity;
     }
 
--- src/main/java/com/infogain/gcp/poc/service/OutboxService.java
+++ src/main/java/com/infogain/gcp/poc/service/OutboxService.java
@@ -1,21 +1,13 @@
 package com.infogain.gcp.poc.service;
 
-import com.google.cloud.Timestamp;
 import com.infogain.gcp.poc.entity.OutboxEntity;
-import com.infogain.gcp.poc.entity.OutboxStatusEntity;
 import com.infogain.gcp.poc.model.OutboxModel;
-import com.infogain.gcp.poc.model.OutboxStatusModel;
 import com.infogain.gcp.poc.repository.OutboxRepository;
 import com.infogain.gcp.poc.repository.OutboxStatusRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.IterableUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -31,7 +23,7 @@ public class OutboxService {
     @Transactional
     public OutboxModel saveOutboxModel(OutboxModel outboxModel){
         OutboxEntity outboxEntity = outboxModel.buildEntity();
-        outboxEntity.setCreated(Timestamp.now());
+        log.info("Saving OutboxEntity={}",outboxEntity.toString());
         outboxEntity = outboxRepository.save(outboxEntity);
         log.info("Saved OutboxEntity={}",outboxEntity.toString());
         return outboxEntity.buildModel();
